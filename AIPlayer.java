4f581f7017a80a0a1763aea57bc8719e7b2079d1
Antonio Ortiz-Morales <81970588+MrOats325@users.noreply.github.com>
2024-01-17 12:58:44 -0500

    Added JavaDoc to AIPlayer
    Describing The Functions

--- AIPlayer.java
+++ AIPlayer.java
@@ -1,12 +1,31 @@
 import java.util.ArrayList;
 import java.util.Map;
 
+/**
+ * This is the AIPlayer Class that will similuate the moves of the simple AI
+ */
 public class AIPlayer {
+    /**
+     * board is an arraylist of the Card Class
+     */
     private ArrayList<Card> board;
+
+    /**
+     * The deck is just a call of an arraylist of cards 
+     */
     private Deck deck;
 
+    /**
+     * A reference which current Board is required to win
+     */
     private int currentBoardRank;
 
+    /**
+     * This is the constructor for AIPlayer which loads the deck
+     * shuffles the deck 
+     * Deals its 4x4 board of 16cards 
+     * @param deck calling the deck of cards
+     */
     public AIPlayer(Deck deck) {
         this.deck = deck;
         this.deck.shuffle();
@@ -32,10 +51,19 @@ public class AIPlayer {
         }
     }
 
+
+    /**
+     * Setting the current rank to the currentBoardRank
+     * @param rank a cursosr for which board is needed to win 
+     */
     public void setCurrentBoardRank(int rank) {
         this.currentBoardRank = rank;
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getCurrentBoardRank() {
         return currentBoardRank;
     }
